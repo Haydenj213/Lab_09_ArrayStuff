138232d7697866dfa783f15984472e9e2025e525
Haydenj213 <johns3hm@mail.uc.edu>
2025-03-13 13:57:52 -0400

    Part 1 Done. Created Array with random numbers 1-100.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Random rnd = new Random();
+
+        int[] dataPoints = new int[100];
+        int sum = 0;
+        double average = 0.0;
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rnd.nextInt(100) + 1;
+        }
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i] + " | ");
+        }
+        for (int i = 0; i < dataPoints.length; i++) {
+            sum += dataPoints[i];
+            average = (double) sum / dataPoints.length;
+        }
+
+        System.out.println("\nThe sum of the random array (dataPoints) is " + sum);
+        System.out.println("The average of the random array (dataPoints) is " + average);
+
     }
 }
